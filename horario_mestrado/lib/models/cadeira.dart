d3194291eda70412d0c39af846a8736d4ecdae27
Miguel Da Silva <infotecjuniorr@gmail.com>
2025-10-02 17:04:17 +0100

    Atualização JSON e BD

--- horario_mestrado/lib/models/cadeira.dart
+++ horario_mestrado/lib/models/cadeira.dart
@@ -3,12 +3,14 @@ class Cadeira {
   final String nome;
   final String sigla;
   final String informacao;
+  final List<String>? professores;
 
   Cadeira({
     required this.cadeiraID,
     required this.nome,
     required this.sigla,
-    this.informacao = "Sem Infromação",
+    this.informacao = "Sem Informação",
+    this.professores = const [],
   });
 
   factory Cadeira.fromMap(Map<String, dynamic> map) => Cadeira(
@@ -16,6 +18,7 @@ class Cadeira {
     nome: map['nome'] ?? "Nome Não Definido",
     sigla: map['sigla'] ?? "Sigla Não Definida",
     informacao: map['informacao'] ?? "Sem Informação",
+    professores: List<String>.from(map['professores'] ?? []),
   );
 
   Map<String, dynamic> toMap() => {
@@ -23,5 +26,6 @@ class Cadeira {
     'nome': nome,
     'sigla': sigla,
     'informacao': informacao,
+    'professores': professores,
   };
 }
